90acadc25ef7694ba1ca042a221792ab0d300fad
Abdullah Asim <abdullahasimoffical4737@gmail.com>
2026-08-31 14:00:34 +0500

    hi

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,7 @@ const loginRouter=require('./routes/login');
 const bodyParser=require('body-parser');
 const cors=require('cors');
 const dotenv=require('dotenv');
-dotenv.Config();
+dotenv.config();
 const CreateAdminAccount=require('./scripts/admin')
 const app=express();
 const port=process.env.PORT || 3000;
